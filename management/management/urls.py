b78a43634111074788b8a86377fed988c9cf4f6f
evtepo <andrey_laguta00@mail.ru>
2024-07-25 23:39:58 +1000

    добавить новые пути/изменить добавление пути для toolbar

--- management/management/urls.py
+++ management/management/urls.py
@@ -1,9 +1,17 @@
 from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
+from django.conf import settings
+from django.contrib.auth.decorators import login_required
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("home/", TemplateView.as_view(template_name="home.html"), name="home"),
+    path("profile/", login_required(TemplateView.as_view(template_name="profile.html")), name="profile"),
     path("auth/", include("authorization.urls")),
-] + debug_toolbar_urls()
+]
+
+if settings.DEBUG:
+    urlpatterns += debug_toolbar_urls()
