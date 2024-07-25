5c4c5a8e58a05dddf25d72c8c471d60304064b82
evtepo <andrey_laguta00@mail.ru>
2024-07-25 23:36:29 +1000

    добавить новые пути

--- management/authorization/urls.py
+++ management/authorization/urls.py
@@ -1,8 +1,14 @@
 from django.contrib.auth import views
+from django.contrib.auth.decorators import login_required
 from django.urls import path
+
+from authorization.views import NewPassView, ProfileView, RegistrationView
 
 
 urlpatterns = [
-    path("login/", views.LoginView.as_view(), name="login"),
-    path("logout/", views.LogoutView.as_view(), name="logout"),
+    path("login/", views.LoginView.as_view(template_name="login.html"), name="login"),
+    path("logout/", login_required(views.LogoutView.as_view()), name="logout"),
+    path("profile/", login_required(ProfileView.as_view()), name="profile"),
+    path("change_password/", login_required(NewPassView.as_view()), name="change_password"),
+    path("signup/", RegistrationView.as_view(), name="signup"),
 ]
