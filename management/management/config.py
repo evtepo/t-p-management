4b45de7d1addecbb1991c70f3c173ab2ef722c66
evtepo <andrey_laguta00@mail.ru>
2024-07-25 23:39:17 +1000

    добавить проверку для debug

--- management/management/config.py
+++ management/management/config.py
@@ -1,15 +1,11 @@
-from dotenv import load_dotenv
 from pydantic import Field
 from pydantic_settings import BaseSettings
 
 
-load_dotenv()
-
-
 class Settings(BaseSettings):
+    allowed_hosts: str = Field(alias="ALLOWED_HOSTS", default="localhost")
+    debug: str = Field(alias="DEBUG", default="false")
     secret_key: str = Field(alias="SECRET_KEY", default="Undefined")
-    debug: bool = bool(Field(alias="DEBUG", default=False))
-
     session_cookie_time: int = Field(alias="COOKIE_TIME", default=14)  #  Pass in days.
 
     db_name: str = Field(alias="DB_NAME", default="management")
@@ -17,6 +13,20 @@ class Settings(BaseSettings):
     db_password: str = Field(alias="DB_PASSWORD", default="postgres")
     db_host: str = Field(alias="DB_HOST", default="localhost")
     db_port: int = Field(alias="DB_PORT", default=5432)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.debug = self.__str_to_bool(self.debug)
+
+    def __str_to_bool(self, val: str | bool) -> bool:
+        if isinstance(val, str):
+            return val.lower() in ("true", "yes", "y")
+        
+        return val
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
 
 
 settings = Settings()
